8620b853aec696db26d56dbe5b8ff3b76ea58104
juanpc10 <juanp23carrillo@gmail.com>
2020-11-16 18:23:52 -0700

    front layout

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
+import React from 'react';
 import './App.css';
+// import InputsForm from './components/InputsForm';
+// import TransactionsList from './components/TransactionsList';
+
+// import { GlobalProvider } from './context/globalState';
+
+
 
 function App() {
   return (
     <>
-      <p>sfdg</p>
-      <p>adsf</p>
+      {/* <GlobalProvider> */}
+        <h2>Bankapp</h2>
+        {/* <InputsForm />
+        <TransactionsList />
+      </GlobalProvider> */}
     </>
   );
 }
